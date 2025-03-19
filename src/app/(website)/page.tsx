851b5e19d96e8541900479899f9d8e3d79a5dcd1
Peter Slovanky <slovanky@gmail.com>
2025-03-20 01:51:39 +0200

    responsive

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -18,17 +18,18 @@ export default function JobsPage() {
 
       {/* Page Content */}
       <div className="flex items-stretch gap-1.5">
-        <div className="grow px-4 lg:px-10 py-2.5 lg:py-6 flex items-end lg:items-center justify-between gap-5 bg-[#3D8E41] text-white rounded-[2px] lg:rounded-[5px]">
+        <div className="relative grow px-4 lg:px-10 py-2.5 lg:py-6 flex items-end lg:items-center justify-between gap-5 bg-[#3D8E41] text-white rounded-[2px] lg:rounded-[5px]">
           <div className="space-y-1">
             <h4 className="text-sm lg:text-[23px] font-bold">UI Designer in Egypt</h4>
             <p className="text-[11px] lg:text-[17px]">70 job positions</p>
           </div>
-          <div className="flex items-center gap-3 text-[11px] lg:text-[21px]">
+
+          <div className="absolute lg:static top-auto left-auto bottom-2.5 right-4 flex items-center gap-3 text-[11px] lg:text-[21px]">
             <span>Set alert</span>
 
             <label className="inline-flex items-center cursor-pointer">
               <input type="checkbox" value="" className="sr-only peer" aria-label="Alert" />
-              <div className="relative w-11 h-6 bg-[#C4C3C3]/50 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-black/10 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#C4C3C3]/50"></div>
+              <div className="relative w-9 lg:w-11 h-5 lg:h-6 bg-[#C4C3C3]/50 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-black/10 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:lg:h-5 after:w-4 after:lg:w-5 after:transition-all peer-checked:bg-[#C4C3C3]/50"></div>
             </label>
           </div>
         </div>
